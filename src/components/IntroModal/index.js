e95320bc0b12c4283bad625a239077a85dfef495
Tyler Corsair <tyler@tylercorsair.com>
2019-10-25 02:43:41 -0700

    Update intro model to include information about open source.

--- src/components/IntroModal/index.js
+++ src/components/IntroModal/index.js
@@ -19,17 +19,17 @@ class IntroModal extends Component {
         }}>
         <img src="/finalicon.svg" className="intro-logo"/>
         <h1>
-          <b>MapleStory:</b> Design
+          <b>MapleStory:</b> Design (v1.0)
           <br/>
           <span className="modal-desc">The <b>unofficial</b> MapleStory simulator and designer.</span>
         </h1>
-        <h3>Latest Updates</h3>
-        <p>Visit our <a href="http://blog.crr.io">blog</a> for more information about our latest updates, new features, and bug fixes on MapleStory: Design.</p>
-        <h3>Have feedback or want to report a bug?</h3>
-        <p>Let us know on our <a href="https://discord.gg/D65Grk9">Discord</a> or by email at support@crr.io.</p>
-        <h3>Disclaimer</h3><span className="avatar-box">{this.getRandomAvatar()}</span>
+        <h3>Hello mapler!</h3>
+        <p>Welcome to our <b>open source</b> version of the service. Please remember to check the <a href="https://maplestory.design" target="_blank">main site</a> for updates and other news.</p>
+        <p>This is an experimental version. If items or characters aren't rendering, please switch from the "latest" Maplestory version to any other available.</p>
+        <h3>Need assistance or have a question?</h3>
+        <p>Join our community on <a href="https://discord.gg/D65Grk9">Discord</a> to ask questions or reach out to our team by <a href="mailto:support@crr.io" target="_blank">email</a> if you need more help.</p>
+        <h3>Disclaimer</h3><span className="avatar-box"><img src="https://maplestory.io/api/gms/latest/character/center/2000/1073181,1053109,1053109,1004862,33005,21544/sit/0"/></span>
         <p>All assets and resources regarding MapleStory thereof are the sole property of <a href="//nexon.net">Nexon</a> and applies to their Terms of Use. By using this service, you agree to respect all copyrights and to not use any assets commercially without permission from Nexon.</p>
-        <p>We are not an official MapleStory server nor fansite; <i>we provide this service for free in the spirit of free-to-play gaming.</i></p>
         <span onClick={this.closeModal.bind(this)} className="btn bg-green text-white intro-dismiss"><i className="fa fa-check"></i> I understand</span>
       </Modal>
     )
@@ -37,24 +37,6 @@ class IntroModal extends Component {
 
   closeModal () {
     this.props.onSetModalOpen(false)
-  }
-
-  getRandomAvatar () {
-    const knownAvatars = [
-      '2000/1073181,1053109,1053109,1004862,33005,21544', // Tyler Corsair (Crrio)
-      '2000/1052923,1052923,1002186,33515,21136', // Andy (Pink Bean's developer)
-      '2001/20305,30130,1050118', // Dray86 (Maple Kombat)
-      '2000/1004776,21645,35707,1042245,1062232', // Paul (Artist and Logo designer)
-    ].map(avatar => `https://maplestory.io/api/gms/latest/character/center/${avatar}/sit/0`).concat(
-      [
-        // Extra characters using the new horrid URL format.
-        ''
-      ]
-    )
-
-    if (!chosenAvatar) chosenAvatar = knownAvatars[Math.floor(Math.random() * knownAvatars.length)]
-
-    return (<img src={chosenAvatar} alt={`avatar`} />)
   }
 }
 
